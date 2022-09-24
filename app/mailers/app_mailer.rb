3060ff46f26666a9688ad5f373cac570ca12aee3
xwiz <c.opata@robosyslive.com>
2022-09-24 14:21:10 +0100

    Update to latest

--- app/mailers/app_mailer.rb
+++ app/mailers/app_mailer.rb
@@ -7,13 +7,13 @@ class AppMailer < ApplicationMailer
 
   def new_user(user)
     @user = user
-    mail :to => @user.email_address, :subject => "Welcome to Postal"
+    mail :to => @user.email_address, :subject => "Welcome to VenMail"
   end
 
   def user_invite(user_invite, organization)
     @user_invite = user_invite
     @organization = organization
-    mail :to => @user_invite.email_address, :subject => "Access the #{organization.name} organization on Postal"
+    mail :to => @user_invite.email_address, :subject => "Access the #{organization.name} organization on VenMail"
   end
 
   def verify_domain(domain, email_address, user)
@@ -26,7 +26,7 @@ class AppMailer < ApplicationMailer
   def password_reset(user, return_to = nil)
     @user = user
     @return_to = return_to
-    mail :to => @user.email_address, :subject => "Reset your Postal password"
+    mail :to => @user.email_address, :subject => "Reset your VenMail password"
   end
 
   def server_send_limit_approaching(server)
@@ -45,7 +45,7 @@ class AppMailer < ApplicationMailer
   end
 
   def test_message(recipient)
-    mail :to => recipient, :subject => "Postal SMTP Test Message"
+    mail :to => recipient, :subject => "VenMail SMTP Test Message"
   end
 
 end
