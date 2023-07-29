a6866d46809177b263b4cecea5608191ed9afc05
xwiz <c.opata@robosyslive.com>
2023-07-29 15:43:07 +0100

    Add domain api

---
+++ api/controllers/domains_api_controller.rb
@@ -0,0 +1,50 @@
+controller :domains do
+    friendly_name "Domains API"
+    description "This API allows you to manage domains"
+    authenticator :server
+
+
+      # Define an action named list
+action :list do
+    # Set the title and description of the action
+    title "List domains"
+    description "Retrieve all available domains for current server"
+    # Return
+    returns Array
+    # Action
+    action do
+      # Find all domains that belong to the current server identity
+      domains = Domain.where(:server_id => identity.server.id)
+      # Return an array of hashes with the domain attributes
+      result[:data] = domains.map do |domain|
+        {
+          :id => domain.id,
+          :name => domain.name,
+          :verification_token => domain.verification_token,
+          :verification_method => domain.verification_method,
+          :verified_at => domain.verified_at,
+          :dkim_private_key => domain.dkim_private_key,
+          :created_at => domain.created_at,
+          :updated_at => domain.updated_at,
+          :dns_checked_at => domain.dns_checked_at,
+          :spf_status => domain.spf_status,
+          :spf_error => domain.spf_error,
+          :dkim_status => domain.dkim_status,
+          :dkim_error => domain.dkim_error,
+          :mx_status => domain.mx_status,
+          :mx_error => domain.mx_error,
+          :return_path_status => domain.return_path_status,
+          :return_path_error => domain.return_path_error,
+          :outgoing => domain.outgoing,
+          :incoming => domain.incoming,
+          :owner_type => domain.owner_type,
+          :owner_id => domain.owner_id,
+          :dkim_identifier_string => domain.dkim_identifier_string,
+          :use_for_any => domain.use_for_any
+        }
+      end
+    end
+  end
+  
+      
+  end
--- api/controllers/routes_api_controller.rb
+++ api/controllers/routes_api_controller.rb
@@ -40,5 +40,30 @@ controller :routes do
       def route_params
         params.permit(:name, :domain_id, :endpoint_id, :endpoint_type, :mode, :spam_mode)
       end
+
+      action :list do
+        # Set the title and description of the action
+        title "List routes"
+        description "Retrieve all available routes for current server"
+        # Return
+        returns Array
+        # Action
+        action do
+          # Find all routes that belong to the current server identity
+          routes = Route.where(:server_id => identity.server.id)
+          # Return an array of hashes with the route attributes
+          result[:data] = routes.map do |route|
+            {
+              :id => route.id,
+              :name => route.name,
+              :domain_id => route.domain_id,
+              :endpoint_id => route.endpoint_id,
+              :endpoint_type => route.endpoint_type,
+              :mode => route.mode,
+              :spam_mode => route.spam_mode
+            }
+          end
+        end
+      end
       
   end
