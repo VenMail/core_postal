106137ba6e9ef0935d8d709fb2e48193090e2d9f
Chibueze Opata <c.opata@robosyslive.com>
2023-07-30 09:12:58 +0100

    Fix add route

--- api/controllers/routes_api_controller.rb
+++ api/controllers/routes_api_controller.rb
@@ -15,11 +15,13 @@ controller :routes do
     param :name, required: true, type: String, desc: "The e-mail address of the recipient (max 255)"
     param :domain_id, required: true, type: Integer, desc: "The id of the domain"
     param :endpoint_id, required: true, type: Integer, desc: "The id of the endpoint"
-    param :endpoint_type, type: String, desc: "The type of the endpoint"
-    param :mode, type: String, desc: "The mode of the route"
-    param :spam_mode, type: String, desc: "The spam mode of the route"
+    param :endpoint_type, type: String, desc: "The type of the endpoint (valid values: 'HttpEndpoint', 'AddressEndpoint', ...)"
+    param :mode, type: String, desc: "The mode of the route (valid values: 'Endpoint', 'Accept', ...)"
+    param :spam_mode, type: String, desc: "The spam mode of the route (valid values: 'Mark', 'Quarantine', ...)"
     # Errors
     error 'RecordInvalid', "The provided data was not sufficient to create a route", attributes: { errors: "A hash of error details" }
+    error 'DomainNotFound', "The domain_id provided does not exist"
+    error 'EndpointNotFound', "The endpoint_id provided does not exist"
     # Return
     returns Hash
     # Action
@@ -27,9 +29,22 @@ controller :routes do
       def route_params
         params.permit(:name, :domain_id, :endpoint_id, :endpoint_type, :mode, :spam_mode)
       end
-    
+
+      # Check if domain_id and endpoint_id exist in the database
+      domain = Domain.find_by(id: route_params[:domain_id])
+      endpoint = Endpoint.find_by(id: route_params[:endpoint_id])
+      raise DomainNotFound unless domain
+      raise EndpointNotFound unless endpoint
+
       result = {}  # Initialize the result variable
-      @route.create(route_params)
+      @route.create(
+        name: route_params[:name],
+        domain_id: route_params[:domain_id],
+        endpoint_id: route_params[:endpoint_id],
+        endpoint_type: route_params[:endpoint_type],
+        mode: route_params[:mode],
+        spam_mode: route_params[:spam_mode]
+      )
       if @route.persisted?
         result[:data] = { id: @route.id, name: @route.name }
       else
@@ -38,6 +53,10 @@ controller :routes do
       result  # Return the result
     rescue ActiveRecord::RecordInvalid => e
       error "RecordInvalid", e.message, errors: e.record.errors
+    rescue DomainNotFound => e
+      error "DomainNotFound", e.message
+    rescue EndpointNotFound => e
+      error "EndpointNotFound", e.message
     end
   end
 
