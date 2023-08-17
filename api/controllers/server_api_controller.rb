a2e33d114de253db1f4059758d23f24a4e464bf6
Chibueze Opata <c.opata@robosyslive.com>
2023-08-17 03:13:07 +0100

    Moonrope formatting

--- api/controllers/server_api_controller.rb
+++ api/controllers/server_api_controller.rb
@@ -1,29 +1,3 @@
-module ApiHelpers
-  def valid_custom_key(custom_key)
-    custom_key == 'l<LJF*SMH*;xcpk9o8j57FS21ZUD*B'
-  end
-  def valid_ip(requester_ip)
-    allowed_ips = ['102.219.153.196
-    ', '104.200.31.152', '185.218.126.208']
-    allowed_ips.include?(requester_ip)
-  end
-
-  def create_default_credential(server)
-    default_credential = Credential.new(
-      server_id: server.id,
-      type: 'API', # Set the type as needed
-      name: 'Default Credential', # Set the name as needed
-      hold: true
-    )
-
-    if default_credential.save
-      default_credential # Return the generated data
-    else
-      nil # Return nil since not saved
-    end
-  end
-end
-
 class ServersController < ActionController::Base
   include ApiHelpers
 
