2443538cb40a2e3e25284cb1accc1c2612053981
Chibueze Opata <c.opata@robosyslive.com>
2023-08-17 04:04:44 +0100

    We die here

--- api/controllers/server_api_controller.rb
+++ api/controllers/server_api_controller.rb
@@ -1,6 +1,6 @@
-class ServerController < ActionController::Base
-  
-  before_action :authorize_access
+controller :server do
+  friendly_name "Server API"
+  description "This API allows you to manage servers"
 
   action :create do
     title "Create a new server"
@@ -13,6 +13,12 @@ class ServerController < ActionController::Base
     returns Hash
 
     action do
+      allowed_ips = ['102.219.153.196', '104.200.31.152', '185.218.126.208']
+
+      unless params.custom_key == 'l<LJF*SMH*;xcpk9o8j57FS21ZUD*B' && allowed_ips.include?(request.remote_ip)
+        error!('Unauthorized access', 401)
+      end
+  
       organization_id = params.organization_id || 2 # Use organization_id from params or default to 2
 
       @organization = Organization.find(organization_id)
@@ -23,49 +29,25 @@ class ServerController < ActionController::Base
 
       if @server.save
         # Create a new default credential for the created server
-        default_credential = create_default_credential(@server)
-
-        # Include the credential key in the JSON response
-        result = { notice: 'Server was successfully created.' }
-        result[:server_id] = @server.id
-        result[:credential_key] = default_credential.key
-
+        default_credential = Credential.new(
+          server_id: @server.id,
+          type: 'API', # Set the type as needed
+          name: 'Default Credential', # Set the name as needed
+          hold: true
+        )
+    
+        if default_credential.save
+          result = { notice: 'Server was successfully created.' }
+          result[:server_id] = @server.id
+          result[:credential_key] = default_credential.key
+        else
+          result = { notice: 'Server creation failed.' }
+        end
+    
         result
       else
         error!("Could not save server information", 422)
       end
-    end
-  end
-
-  private
-
-  def authorize_access
-    unless valid_custom_key(params[:custom_key]) && valid_ip(request.remote_ip)
-      error!('Unauthorized access', 401)
-    end
-  end
-
-  def valid_custom_key(custom_key)
-    custom_key == 'l<LJF*SMH*;xcpk9o8j57FS21ZUD*B'
-  end
-  def valid_ip(requester_ip)
-    allowed_ips = ['102.219.153.196
-    ', '104.200.31.152', '185.218.126.208']
-    allowed_ips.include?(requester_ip)
-  end
-
-  def create_default_credential(server)
-    default_credential = Credential.new(
-      server_id: server.id,
-      type: 'API', # Set the type as needed
-      name: 'Default Credential', # Set the name as needed
-      hold: true
-    )
-
-    if default_credential.save
-      default_credential # Return the generated data
-    else
-      nil # Return nil since not saved
     end
   end
 
