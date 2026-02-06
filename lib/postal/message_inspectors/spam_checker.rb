6a27d17f97806c67f1e59c1e195558c1b28501af
Chibueze Opata <copata@venmail.io>
2026-02-06 10:35:31 +0100

    Add custom inspector

---
+++ lib/postal/message_inspectors/spam_checker.rb
@@ -0,0 +1,36 @@
+module Postal
+  module MessageInspectors
+    # Wrapper inspector to run the local SpamChecker for every message
+    class SpamChecker < MessageInspector
+      def inspect_message(inspection)
+        message = inspection.message
+        raw_message = message.raw_message
+
+        headers_hash = message.headers || {}
+        headers_array = headers_hash.flat_map do |key, values|
+          Array(values).map { |value| "#{key}: #{value}" }
+        end
+        subject = Array(headers_hash['subject']).last.to_s
+
+        spam_score = Postal::SpamChecker.classify_email(
+          message.mail_from,
+          raw_message,
+          headers_array,
+          subject
+        )
+
+        return unless spam_score
+
+        if spam_score > 20
+          inspection.spam_checks << SpamCheck.new("V_SPAM", spam_score, "Message classified as spam")
+        elsif spam_score > 5
+          inspection.spam_checks << SpamCheck.new("V_SPAM", spam_score / 2.0, "Venmail pre-classifier spam score")
+        end
+      rescue => e
+        logger.error "Error running internal spam checker: #{e.class} (#{e.message})"
+        logger.error e.backtrace[0, 5] if e.backtrace
+        inspection.spam_checks << SpamCheck.new("ERROR", 0, "Error when scanning with internal spam checker")
+      end
+    end
+  end
+end
